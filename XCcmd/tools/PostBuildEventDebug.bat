40638cb6407d6bdc27d63783c813740f34b766dd
alexchartrand <alexchartrand91@gmail.com>
2016-10-24 17:24:50 -0400

    Add file

---
+++ XCcmd/tools/PostBuildEventDebug.bat
@@ -0,0 +1,10 @@
+rem Copie et renomme les fichiers lib et pdb. 
+rem
+rem Le nom inclue le suffixe _d pour debug.
+
+echo.
+echo Copying debug library files ...
+echo.
+
+copy "..\x64\Debug\XCcmd.exe" "..\bin\XCcmd_x64d.exe"
+rem copy "..\x64\Debug\XtractCcmd.pdb" "..\bin\XtractCcmd_x64d.pdb"
